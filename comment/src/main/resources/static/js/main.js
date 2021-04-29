b7bd3ccddf4d3e0795e476661e7216e6dc9c3f4a
ss-hoon <lshoon1128@naver.com>
2021-04-29 17:06:20 +0900

    빈칸 검사 추가

--- comment/src/main/resources/static/js/main.js
+++ comment/src/main/resources/static/js/main.js
@@ -3,13 +3,19 @@ $(document).ready(function(){
 	/* 댓글 작성 버튼을 눌렀을 때의 이벤트 */
 	$("#btnWrite").click(function(){
 		
-		// 작성한 댓글과 작성자로 객체 생성
+		/* 빈칸 검사 */
+		if($("#commentContents").val() == '' || $("#commentWriter").val() == ''){
+			alert("댓글 또는 작성자를 1자 이상 작성해주세요");
+			return;
+		}
+		
+		/* 작성한 댓글과 작성자로 객체 생성 */
 		var param = {
 			"contents" : $("#commentContents").val(),
 			"userId" : $("#commentWriter").val()
 		};
 		
-		// param 객체를 JSON 문자열로 변환하여 AJAX 통신
+		/* param 객체를 JSON 문자열로 변환하여 AJAX 통신 */
 		$.ajax({
 			url : "/insert",
 			type : "POST",
